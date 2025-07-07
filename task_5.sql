1c51ae4abec2f5c6957d7843ca3d1a615160ea67
21407alfredmunga <21407alfredmunga@gmail.com>
2025-07-07 20:20:14 +0300

    Refactor task_5.sql to correct table name and update address value for customer insertion

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
--- Script to insert a single row into the customers table
--- in the alx_book_store database
--- The database name will be passed as argument to mysql command
+-- task_5.sql
+-- Script to insert a single row into the customer table
+-- in the database passed as an argument to the mysql command
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', 'cbaidoo@sandtech.com');
